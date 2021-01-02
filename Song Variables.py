49ea12e63cd18899f3a1bce1845ae60c5f6b2538
samsnarrl22 <76850000+samsnarrl22@users.noreply.github.com>
2021-01-02 21:54:53 +0000

    Add files via upload
    Storing and printing song variables in python

---
+++ Song Variables.py
@@ -0,0 +1,31 @@
+# Storing attributes about a song in Variables
+
+# First create variables for each of the characteristics that make up a song
+title = "Ring of Fire"
+artist = "Jonny Cash"  # These variables are strings which can be seen due to the ""
+album = "The Best of Johnny Cash"
+genre = "Country"
+time_in_seconds = 163  # time_in_seconds is an integer
+beats_per_minute = 103.5  # beats_per_minute is a float
+track_number = "1"  # These variables will appear like integers but are strings
+year_released = "1967"
+
+# Now its time to print these variables so we can see them in the console
+print(title)
+print(artist)
+print(album)
+print(genre)
+print(time_in_seconds)
+print(beats_per_minute)
+print(track_number)
+print(year_released)
+"""This shows us the information stored in the variable.
+However it is just a random list at this point so it would like better if they were defined"""
+print("Title: ", title)
+print("Artist: ", artist)
+print("Album: ", album)
+print("Genre: ", genre)
+print("Track length (s):  ", time_in_seconds)
+print("BPM", beats_per_minute)
+print("Track Number: ", track_number)
+print("Year Released: ", year_released)
